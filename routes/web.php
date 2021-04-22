41d6a480a7496a281b483912660f782ae5945591
Dandy Inot <dandy.inot1990@gmail.com>
2021-04-22 18:00:21 +0800

    Add frontend template, setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\WebsiteController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +25,16 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/', [WebsiteController::class, 'index'])->name('index');
 Route::get('/category/{slug}', [WebsiteController::class, 'category'])->name('category');
+Route::get('/post/{slug}',[WebsiteController::class, 'post'])->name('post');
+Route::get('/page/{slug}', [WebsiteController::class, 'page'])->name('page');
+Route::get('/contact/create', [WebsiteController::class, 'createContact'])->name('contact.create');
+Route::post('/contact', [WebsiteController::class, 'storeContact'])->name('contact.store');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+
+    Route::resources([
+        'categories' => CategoryController::class,
+        'posts' => PostController::class,
+        'galleries' => GalleryController::class,
+    ]);
+});
