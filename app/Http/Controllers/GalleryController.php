fd3c6af2ba399979b6c17d206b7df5e0789874e0
Dandy Inot <dandy.inot1990@gmail.com>
2021-04-27 17:57:26 +0800

    Ongoing Gallery CRUD

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
-    //
+    public function index(){
+
+        $galleries = Gallery::latest()->get();
+
+        return view('backend.galleries.index', compact('galleries'));
+    }
+
+    public function create(){
+
+        return view('backend.galleries.create');
+    }
+
+    public function store(Request $request){
+
+        $this->validate($request,[
+            'image_url' => 'required'
+        ],
+            [
+                'image_url.required' => 'Select image'        
+            ]
+        );
+
+        foreach($request->image_url as $image_url){
+
+            $fileNameWithExt = $image_url->getClientOriginalName();
+
+            //just get file name
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            //just get extension
+            $fileExt = $image_url->getClientOrginalExtension();
+
+            $fileNameToStore = $fileName . '_' . $fileExt;
+
+            $gallery = new Gallery();
+            $gallery->image_url = $fileNameToStore;
+            $gallery->user_id = Auth::user()->id;
+            $save = $gallery->saveMany();
+
+            if($save){
+                $image_url->storeAs('public/galleries', $fileNameToStore);
+            }
+        }
+
+        
+        session()->flash('message', 'Images successfully uploaded');
+        return redirect()->route('galleries.index');
+    }
 }
---
+++ resources/views/backend/galleries/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card mb-4">
+                <div class="card-header">
+                    {{ __('Create New Gallery') }}
+                </div>
+
+                <div class="card-body">
+                    <form action="{{route('galleries.store')}}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <label for="image_url">Select Image</label>
+                            <input type="file" name="image_url" class="form-control-file @error('image_url') is-invalid @enderror" multiple>
+
+                            @error('image_url')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+
+                        </div>
+                        
+                        <div class="form-group">
+                            <input type="submit" value="Save" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
