28d35f36ec856f1b3300bd803b17cdfaa1948723
Dandy Inot <dandy.inot1990@gmail.com>
2021-04-28 15:50:14 +0800

    done Gallery CRUD

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Gallery;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class GalleryController extends Controller
 {
@@ -36,22 +38,73 @@ class GalleryController extends Controller
             //just get file name
             $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
             //just get extension
-            $fileExt = $image_url->getClientOrginalExtension();
+            $fileExt = $image_url->getClientOriginalExtension();
 
-            $fileNameToStore = $fileName . '_' . $fileExt;
+            $fileNameToStore = $fileName . '.' . $fileExt;
 
             $gallery = new Gallery();
             $gallery->image_url = $fileNameToStore;
             $gallery->user_id = Auth::user()->id;
-            $save = $gallery->saveMany();
+            $save = $gallery->save();
 
             if($save){
+
                 $image_url->storeAs('public/galleries', $fileNameToStore);
             }
+            
         }
 
         
         session()->flash('message', 'Images successfully uploaded');
+        return redirect()->route('galleries.index');
+    }
+
+    public function edit(Gallery $gallery){
+
+        return view('backend.galleries.edit', compact('gallery'));
+    }
+
+    public function update(Request $request, Gallery $gallery){
+        
+        $this->validate($request,[
+            'image_url' => 'required'
+        ],
+            [
+                'image_url.required' => 'Select image'        
+            ]
+        );
+
+        $fileNameWithExt = $request->image_url->getClientOriginalName();
+
+        //just get file name
+        $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+        //just get extension
+        $fileExt = $request->image_url->getClientOriginalExtension();
+
+        $fileNameToStore = $fileName . '.' . $fileExt;
+
+        $gallery->image_url = $fileNameToStore;
+        $gallery->user_id = Auth::user()->id;
+        $save = $gallery->save();
+
+        if($save){
+
+            $request->image_url->storeAs('public/galleries', $fileNameToStore);
+        }
+            
+
+        session()->flash('message', 'Images successfully updated');
+        return redirect()->route('galleries.index');
+
+    }
+
+    public function destroy(Gallery $gallery){
+
+        Storage::delete('public/galleries/'. $gallery->image_url);
+        $gallery->delete();
+
+        session()->flash('delete-message', 'Gallery successfully deleted');
+
         return redirect()->route('galleries.index');
     }
 }
--- resources/views/backend/galleries/edit.blade.php
+++ resources/views/backend/galleries/edit.blade.php
@@ -6,16 +6,17 @@
         <div class="col-md-12">
             <div class="card mb-4">
                 <div class="card-header">
-                    {{ __('Create New Gallery') }}
+                    {{ __('Gallery - Edit') }}
                 </div>
 
                 <div class="card-body">
-                    <form action="{{route('galleries.store')}}" method="post" enctype="multipart/form-data">
+                    <form action="{{route('galleries.update', $gallery)}}" method="post" enctype="multipart/form-data">
                         @csrf
+                        @method('PUT')
                         <div class="form-group">
+                            <p><img src="{{ asset('storage/galleries/'. $gallery->image_url)}}" alt="" style="width:100px;"></p>
                             <label for="image_url">Select Image</label>
-                            <input type="file" name="image_url[]" class="form-control-file @error('image_url') is-invalid @enderror" multiple>
-
+                            <input type="file" name="image_url" class="form-control-file @error('image_url') is-invalid @enderror" multiple>
                             @error('image_url')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -25,7 +26,7 @@
                         </div>
                         
                         <div class="form-group">
-                            <input type="submit" value="Save" class="btn btn-primary">
+                            <input type="submit" value="Update" class="btn btn-primary">
                         </div>
                     </form>
                 </div>
